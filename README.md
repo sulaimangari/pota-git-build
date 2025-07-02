59d8aed71ffaf1497975682c6b08d827bf086938
sulaimangari <sulaimangari97@gmail.com>
2025-07-02 09:05:49 +0700

    add initial script

---
+++ README.md
@@ -0,0 +1,32 @@
+# :potato: Pota git Build
+## Because my spec too potato too build
+
+This is more of a bandaid implementetaion direct tunneling via terminal with webhook using webhook as initiator for connection.  
+
+The advantage of using this is more integrated environtment with your terminal.  
+
+For now it will be more focused for IoT project (eg. PlatformIO, ESP-IDF, NRF)
+
+## Install
+```bash
+
+# COMING SOON
+
+```
+
+## TODO
+
+- Webhook catcher
+- More integrated environtment with 
+
+## Acknowledgement
+
+This is inspired by:
+- [Tmate](https://github.com/marketplace/actions/debugging-with-tmate)
+
+Thanks to:
+- [Bore](https://github.com/ekzhang/bore/)
+
+- [Smee](https://smee.io)
+
+
--- init.sh
+++ init.sh
@@ -1,13 +1,58 @@
 #!/usr/bin/bash
 
 SMEE_URL=$(curl https://smee.io/new | grep -o "https://smee.io/[A-Za-z0-9-]*")
-gh secret set MYSECRET --body "$SMEE_URL"
+gh secret set SMEE_URL --body "$SMEE_URL"
 
 PUB_KEY=$(cat ~/.ssh/id_*.pub)
-gh secret set MYSECRET --body "$PUB_KEY"
+gh secret set PUB_KEY --body "$PUB_KEY"
 
+mkdir -p .github/workflows
 
+touch .github/workflows/build.yaml
 
+cat <<'EOF' >> .github/workflows/build.yaml
+name: build
 
+on: workflow_dispatch
 
+jobs:
+  build:
+    runs-on: ubuntu-latest
 
+    steps:
+    - name: Install Dependency
+      run: |
+        sudo apt update
+        sudo apt install jo net-tools
+        URL="https://api.github.com/repos/ekzhang/bore/releases/latest"
+        curl -s $URL | awk -F\" '/browser_download_url.*x86_64-unknown-linux-musl.tar.gz/{system("curl -OL " $(NF-1))}'
+        tar -xvf *.tar.gz
+        sudo cp bore /usr/bin/bore
+        exec bash
+
+    - name: Transfer Secret
+      env: 
+        SMEE_URL : ${{ secrets.AUTHORIZED_KEY }}
+        PUB_KEY : ${{ secrets.PUB_KEY }}
+      run: |
+        mkdir ~/.ssh
+        chmod 700 ~/.ssh
+        echo $PUB_KEY >> ~/.ssh/authorized_keys
+        chmod 600 ~/.ssh/authorized_keys
+
+    - name: Create Tunnel
+      run: |
+        nohup bore local 22 --to bore.pub &
+        BORE_PORT=$(cat nohup.out | grep -oP 'bore.pub:\s*\K\d+')
+        while true; do
+            jo bore=$BORE_PORT | curl -X POST -H "Content-Type: application/json" -d @- $SMEE_URL
+
+            if netstat | grep ssh | head -n 1 ; then 
+              break
+            else 
+              sleep 1
+            fi
+        done
+EOF
+
+curl https://smee.io/ShpRVaKYRBLWStXS -H "accept: application/json" 
